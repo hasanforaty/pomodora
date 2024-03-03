59b86855d18cb23d4b890977d1bfd6da066c941f
hasan foraty <hasanforaty19@gmail.com>
2024-03-03 13:00:49 +0330

    add all widgets

--- main.py
+++ main.py
@@ -21,10 +21,35 @@ CHECK_MARK = "✔"
 windows = tk.Tk()
 windows.title("Pomodoro App")
 windows.config(padx=100, pady=50, bg=YELLOW)
+
+# Timer
+timer = tk.Label(text='Timer', font=(FONT_NAME, 80, 'bold'), fg=GREEN, bg=YELLOW)
+timer.grid(row=0, column=1)
+
+# Canvas (Potato)
 canvas = tk.Canvas(windows, width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = tk.PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
 canvas.create_text(100, 130, text="00:00", fill='white', font=(FONT_NAME, 35, 'bold'))
-canvas.pack()
+canvas.grid(row=1, column=1)
+
+
+# start and reset Button
+def start_timer():
+    pass
+
+
+def stop_timer():
+    pass
+
+
+start_button = tk.Button(text="Start", command=start_timer, font=(FONT_NAME, 10, 'bold'), highlightthickness=0)
+start_button.grid(row=2, column=0)
+stop_button = tk.Button(text="Stop", command=stop_timer, font=(FONT_NAME, 10, 'bold'), highlightthickness=0)
+stop_button.grid(row=2, column=2)
+
+# add checkmark
+checkmark_text = tk.Label(text=CHECK_MARK, font=(FONT_NAME, 30, 'bold'), fg=GREEN, bg=YELLOW)
+checkmark_text.grid(row=3, column=1)
 
 windows.mainloop()
