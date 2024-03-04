6abb4481077d8809a206ee67c32ddbfda3554185
hasan foraty <hasanforaty19@gmail.com>
2024-03-04 13:40:52 +0330

    complete Timer time

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 CHECK_MARK = "✔"
+reps = 0
 
 # ---------------------------- TIMER RESET ------------------------------- #
 
@@ -25,6 +26,8 @@ def count_down(count):
         canvas.itemconfig(count_down_text, text=f"{minutes:02d}:{seconds:02d}")
         if count > 0:
             windows.after(1000, count_down, count - 1)
+        else:
+            start_timer()
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -34,8 +37,8 @@ windows.title("Pomodoro App")
 windows.config(padx=100, pady=50, bg=YELLOW)
 
 # Timer
-timer = tk.Label(text='Timer', font=(FONT_NAME, 80, 'bold'), fg=GREEN, bg=YELLOW)
-timer.grid(row=0, column=1)
+timer_label = tk.Label(text='Timer', font=(FONT_NAME, 80, 'bold'), fg=GREEN, bg=YELLOW)
+timer_label.grid(row=0, column=1)
 
 # Canvas (Potato)
 canvas = tk.Canvas(windows, width=200, height=224, bg=YELLOW, highlightthickness=0)
@@ -48,8 +51,19 @@ canvas.grid(row=1, column=1)
 # start and reset Button
 def start_timer():
     global timer_is_running
+    global reps
     timer_is_running = True
-    count_down(WORK_MIN * 60)
+    reps += 1
+    if reps % 8 == 0:
+        time = LONG_BREAK_MIN
+        timer_label.config(text='Long break', fg=RED)
+    elif reps % 2 == 0:
+        time = SHORT_BREAK_MIN
+        timer_label.config(text='Short break', fg=PINK)
+    else:
+        time = WORK_MIN
+        timer_label.config(text='Work time', fg=GREEN)
+    count_down(time * 60)
 
 
 def stop_timer():
@@ -57,9 +71,9 @@ def stop_timer():
     timer_is_running = False
 
 
-start_button = tk.Button(text="Start", command=start_timer, font=(FONT_NAME, 10, 'bold'), highlightthickness=0)
+start_button = tk.Button(text="Start", command=start_timer, font=(FONT_NAME, 10, 'bold'), highlightthickness=1)
 start_button.grid(row=2, column=0)
-stop_button = tk.Button(text="Stop", command=stop_timer, font=(FONT_NAME, 10, 'bold'), highlightthickness=0)
+stop_button = tk.Button(text="Stop", command=stop_timer, font=(FONT_NAME, 10, 'bold'), highlightthickness=1)
 stop_button.grid(row=2, column=2)
 
 # add checkmark
